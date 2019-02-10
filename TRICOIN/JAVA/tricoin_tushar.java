fe9902cc9cf370f1f30d6a555ec4c9677956cfcf
Tushar Kadam <tushar.kadam@somaiya.edu>
2019-02-10 23:07:14 +0530

    Explanation added

--- TRICOIN/JAVA/tricoin_tushar.java
+++ TRICOIN/JAVA/tricoin_tushar.java
@@ -5,6 +5,7 @@ class tricoin_tushar{
         int t=sc.nextInt();
         while(t-->0){
             int num=sc.nextInt();
+            //  lowest postive integer near positve of root of quadratic equation (-b + sqrt( b^2 - 4ac )/2a is the answer ! 
             int ans=(int)((-1.0+Math.sqrt(1+8.0*num))/2.0);
             System.out.println(ans);
         }

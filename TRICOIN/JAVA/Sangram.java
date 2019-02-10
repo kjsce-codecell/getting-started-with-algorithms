489c7e21e4a06e57ddf16d450c0a73f7b9052ddc
Sangram S Desai <sangram.desai@somaiya.edu>
2019-02-10 23:45:01 +0530

    Update Sangram.java

--- TRICOIN/JAVA/Sangram.java
+++ TRICOIN/JAVA/Sangram.java
@@ -5,6 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
+/*Sol: n*(n+1)/2 is total*/
 class Sangram
 {
 	public static void main (String[] args) throws java.lang.Exception
